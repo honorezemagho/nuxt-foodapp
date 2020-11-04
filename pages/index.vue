53bd77cf649707ad1e979b6506a09da6f38ab565
honorezemagho <honorezemagho@gmail.com>
2020-11-04 17:20:48 +0100

    add filter and some ui updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,7 @@
 <template>
-<main>
-    <div class="container">
-        <AppHeader />
-    </div>
-    <AppRestaurantInfo />
+<main class="container">
+    <AppHeader />
+    <AppRestaurantInfo :datasource="fooddata" />
 </main>
 </template>
 
@@ -15,46 +13,12 @@ export default {
         AppHeader,
         AppRestaurantInfo
     },
+    computed: {
+        fooddata() {
+            return this.$store.state.fooddata;
+        },
+    },
 }
 </script>
 
-<style>
-.container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-}
-
-.title {
-    font-family:
-        'Quicksand',
-        'Source Sans Pro',
-        -apple-system,
-        BlinkMacSystemFont,
-        'Segoe UI',
-        Roboto,
-        'Helvetica Neue',
-        Arial,
-        sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-}
-
-.subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-}
-
-.links {
-    padding-top: 15px;
-}
-</style>
+<style></style>
