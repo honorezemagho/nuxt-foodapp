1c1de615302431ed80323cec98a8b0be8d6a59ee
honorezemagho <honorezemagho@gmail.com>
2020-11-12 17:22:16 +0100

    add logo to the menu

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -2,7 +2,9 @@
 <nav>
     <ul>
         <li>
-            <nuxt-link exact to="/">Home</nuxt-link>
+            <nuxt-link exact to="/">
+                <AppLogo />
+            </nuxt-link>
         </li>
         <li>
             <nuxt-link to="/restaurants">Restaurant</nuxt-link>
@@ -13,8 +15,11 @@
 </template>
 
 <script>
+import AppLogo from "@/components/AppLogo.vue";
 export default {
-
+    components: {
+        AppLogo
+    }
 }
 </script>
 
